a0351c0856efe26ea466ccca5c451ee01543769e
James Rogers <jarogers095@gmail.com>
2020-01-28 07:03:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,12 +8,12 @@ def summon_captain_planet(array)
   array.collect do |element|
     element.capitalize + "!"
   end
-  
-  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.all? do |element|
+    element.length < 5
+  end
 end
 
 def find_the_cheese# code an argument here
